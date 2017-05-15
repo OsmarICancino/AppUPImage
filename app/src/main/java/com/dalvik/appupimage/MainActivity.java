d31db90e065959b7337a460536e95457ebfd6dc6
Osmar Cancino <osmar_icancino@MacBook-Air-de-Osmar.local>
2017-05-14 19:21:42 -0500

    Add all COde

--- app/src/main/java/com/dalvik/appupimage/MainActivity.java
+++ app/src/main/java/com/dalvik/appupimage/MainActivity.java
@@ -1,13 +1,187 @@
 package com.dalvik.appupimage;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Hashtable;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Declarando objetos Visuales
+    private Button buttonChoose;
+    private Button buttonUpload;
+    private ImageView imageView;
+    private EditText editTextName;
+    private Bitmap bitmap;
+
+    private int PICK_IMAGE_REQUEST = 1;
+
+    private String UPLOAD_URL ="Reemplaza con tu URL";
+    private String KEY_IMAGE = "image";
+    private String KEY_NAME = "name";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Declaramos las instancias del View
+
+        buttonChoose = (Button) findViewById(R.id.buttonChoose);
+        buttonUpload = (Button) findViewById(R.id.buttonUpload);
+        editTextName = (EditText) findViewById(R.id.editText);
+
+
+        imageView  = (ImageView) findViewById(R.id.imageView);
+
+        buttonChoose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showFileChooser();
+            }
+        });
+
+        buttonUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uploadImage();
+            }
+        });
+
     }
+
+    //Funcion para obtener la imagen del disositivo
+    private void showFileChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Selecciona una imagen"), PICK_IMAGE_REQUEST);
+    }
+
+    //Funcion para obtener el String Base 64 de la imagen
+    public String getStringImage(Bitmap bmp){
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bmp.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] imageBytes = baos.toByteArray();
+        String encodedImage = Base64.encodeToString(imageBytes, Base64.DEFAULT);
+        return encodedImage;
+    }
+
+    //Funcion para subir la imagen con la cola de peticiones volley
+    private void uploadImage(){
+        //Showing the progress dialog
+        final ProgressDialog loading = ProgressDialog.show(this,"Subiendo...","Espere por favor...",false,false);
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, UPLOAD_URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String s) {
+                        //Disimissing the progress dialog
+                        loading.dismiss();
+                        //mostrando mensaje de respuesta
+                        Toast.makeText(MainActivity.this, s , Toast.LENGTH_LONG).show();
+                        createDialog("Imagen subida coorectamente.", true);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError volleyError) {
+                        //Dismissing the progress dialog
+                        loading.dismiss();
+
+                        //Mostrando Mensaje
+                        createDialog("Error al subir la imagen", false);
+                        //Toast.makeText(MainActivity.this, volleyError.getMessage().toString(), Toast.LENGTH_LONG).show();
+
+                    }
+                }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                //Converttiendo Bitmap a String
+                String image = getStringImage(bitmap);
+
+                //Obteniendo nombre de la imagen
+                String name = editTextName.getText().toString().trim();
+
+                //Creando parametros
+                Map<String,String> params = new Hashtable<String, String>();
+
+                //Agregando parametros
+                params.put(KEY_IMAGE, image);
+                params.put(KEY_NAME, name);
+
+                //Regresando parámetros
+                return params;
+            }
+        };
+
+        //Creating a Request Queue
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        //Adding request to the queue
+        requestQueue.add(stringRequest);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            Uri filePath = data.getData();
+            try {
+                //la imagen obtenita es casteada a Bitmap
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                //Colocamos en el ImageView el objeto Bitmap Resultante
+                imageView.setImageBitmap(bitmap);
+
+            } catch (IOException e) {
+                Toast.makeText(this, "Error al instanciar el objeto obtenido.", Toast.LENGTH_SHORT).show();
+                e.printStackTrace();
+            }
+        }
+    }
+
+    //Funcion para desplegar dialogo
+    public void createDialog(String message, boolean flag)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Resultado");
+        if(flag)
+            builder.setIcon(getResources().getDrawable(R.drawable.pacman));
+        else
+            builder.setIcon(getResources().getDrawable(R.drawable.ghost));
+        builder.setMessage(message)
+                .setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // FIRE ZE MISSILES!
+                    }
+                });
+        // Create the AlertDialog object and return it
+        builder.create().show();
+    }
+
+
 }
